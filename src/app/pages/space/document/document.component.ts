f381d9682687c8ea77bbd93649fd63b61015d9dd
ksmnsmarts <ksm@nsmarts.co.kr>
2022-03-02 15:13:42 +0900

    update css

--- src/app/pages/space/document/document.component.ts
+++ src/app/pages/space/document/document.component.ts
@@ -38,6 +38,9 @@ export class DocumentComponent implements OnInit, AfterViewInit {
 	docId: any;
 	isSpaceAdmin: boolean;
 
+    rightBlockDisplay= false;
+    matIcon = 'arrow_back_ios'
+
 	constructor(
 		private route: ActivatedRoute,
 		private router: Router,
@@ -211,4 +214,17 @@ export class DocumentComponent implements OnInit, AfterViewInit {
 			data: editorData
 		});
 	}
+
+    
+    
+    rightBlockDisplayBtn() {
+        if(this.rightBlockDisplay == false){
+            this.rightBlockDisplay = true;
+            this.matIcon = 'arrow_forward_ios'
+        } else {
+            this.rightBlockDisplay = false;
+            this.matIcon = 'arrow_back_ios'
+        }
+        
+    }
 }
