c8a0f5d2ddd298f03e5ed0792375b7f7350fd759
NuriSeol <qqq4020@naver.com>
2023-07-24 13:25:43 +0900

    aos

---
+++ src/app/pages/index/index.component copy.ts
@@ -0,0 +1,95 @@
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import Splitting from 'splitting';
+import AOS from 'aos'; //AOS - 1
+
+declare var Splitting: any;
+
+@Component({
+    selector: 'app-index',
+    templateUrl: './index.component.html',
+    styleUrls: ['./index.component.scss'],
+})
+export class IndexComponent implements OnInit, AfterViewInit {
+    // header
+    isHeaderActive: boolean = false;
+    prevScrollTop: number = 0;
+
+    // scrolla event
+    // isVisualActive: boolean = true;
+    // isContentsActive: boolean = true;
+    // isworksCardsActive: boolean = false;
+
+    // @ViewChild 데코레이터를 사용하여 변수에 ElementRef를 할당
+    // @ViewChild('wrapRef', { static: true }) wrapRef: ElementRef;
+    // @ViewChild('visualRef', { static: true }) visualRef: ElementRef;
+    // @ViewChild('contentsRef', { static: true }) contentsRef: ElementRef;
+    // @ViewChild('worksCardRef', { static: true }) worksCardRef: ElementRef;
+    // @ViewChild('attendanceRef', { static: true }) attendanceRef: ElementRef;
+    // @ViewChild('securityRef', { static: true }) securityRef: ElementRef;
+    // @ViewChild('serviceRef', { static: true }) serviceRef: ElementRef;
+    // @ViewChild('freeCardRef', { static: true }) freeCardRef: ElementRef;
+    // @ViewChild('descRef', { static: true }) descRef: ElementRef;
+
+    // visualTop;
+    // contentsTop;
+    // worksCardTop;
+
+    constructor() {}
+
+    ngOnInit() {
+        // 섹션별 클래스 추가와 제거
+        // nativeElement.getBoundingClientRect() 요소의 좌표값을 찾아줌.
+        // this.visualTop = this.wrapRef.nativeElement.getBoundingClientRect().top;
+        // this.contentsTop = this.contentsRef.nativeElement.getBoundingClientRect().top;
+        // this.worksCardTop = this.worksCardRef.nativeElement.getBoundingClientRect().top;
+
+        AOS.init();
+    }
+
+    ngAfterViewInit(): void {
+        const textToSplit = document.getElementsByClassName('textToSplit');
+        Splitting({ target: textToSplit });
+    }
+
+    @HostListener('scroll', ['$event'])
+    onScroll(event) {
+        const nowScrollTop = event.target.scrollTop || event.target.scrollY || 0;
+        if (nowScrollTop > this.prevScrollTop) {
+            this.isHeaderActive = true;
+        } else {
+            this.isHeaderActive = false;
+        }
+
+        // this.isVisualActive = nowScrollTop === this.visualTop;
+        // console.log('this.isVisualActive 클래스 네임 변경', this.isVisualActive);
+        // console.log('visualTop:', this.visualTop);
+
+        // this.isContentsActive = nowScrollTop === this.contentsTop;
+        // console.log('this.isContentsActive 클래스 네임 변경', this.isContentsActive);
+        // console.log('contentsTop:', this.contentsTop);
+
+        // this.isworksCardsActive = nowScrollTop === this.worksCardTop;
+        // console.log('this.isworksCardsActive 클래스 네임 변경', this.isworksCardsActive);
+        // console.log('worksCardTop:', this.worksCardTop);
+
+        // console.log('nowScrollTop', nowScrollTop);
+
+        // 정운님
+        // this.isVisualActive = this.isElementInViewport(this.visualRef.nativeElement);
+        // console.log('this.isVisualActive 클래스 네임 변경', this.isVisualActive);
+
+        // this.isContentsActive = this.isElementInViewport(this.contentsRef.nativeElement);
+        // console.log('this.isContentsActive 클래스 네임 변경', this.isContentsActive);
+
+        // this.isworksCardsActive = this.isElementInViewport(this.worksCardRef.nativeElement);
+        // console.log('this.isworksCardsActive 클래스 네임 변경', this.isworksCardsActive);
+
+        this.prevScrollTop = nowScrollTop;
+    }
+
+    // 요소의 뷰포트 진입 여부 확인하는 함수
+    // isElementInViewport(el: HTMLElement): boolean {
+    //     const rect = el.getBoundingClientRect();
+    //     return rect.top <= window.innerHeight && rect.bottom >= 0;
+    // }
+}
--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,49 +1,19 @@
-import { AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
-import Splitting from 'splitting';
-declare var Splitting: any;
+import { Component, HostListener, OnInit} from '@angular/core';
+import AOS from 'aos'; //AOS - 1
 
 @Component({
     selector: 'app-index',
     templateUrl: './index.component.html',
     styleUrls: ['./index.component.scss'],
 })
-export class IndexComponent implements OnInit, AfterViewInit {
+
+export class IndexComponent implements OnInit {
     // header
     isHeaderActive: boolean = false;
     prevScrollTop: number = 0;
 
-    // scrolla event
-    isVisualActive: boolean = true;
-    isContentsActive: boolean = true;
-    isworksCardsActive: boolean = false;
-
-    // @ViewChild 데코레이터를 사용하여 변수에 ElementRef를 할당
-    @ViewChild('wrapRef', { static: true }) wrapRef: ElementRef;
-    @ViewChild('visualRef', { static: true }) visualRef: ElementRef;
-    @ViewChild('contentsRef', { static: true }) contentsRef: ElementRef;
-    @ViewChild('worksCardRef', { static: true }) worksCardRef: ElementRef;
-    @ViewChild('attendanceRef', { static: true }) attendanceRef: ElementRef;
-    @ViewChild('securityRef', { static: true }) securityRef: ElementRef;
-    @ViewChild('serviceRef', { static: true }) serviceRef: ElementRef;
-    @ViewChild('freeCardRef', { static: true }) freeCardRef: ElementRef;
-    @ViewChild('descRef', { static: true }) descRef: ElementRef;
-
-    visualTop;
-    contentsTop;
-    worksCardTop;
-    constructor() {}
-
     ngOnInit() {
-        // 섹션별 클래스 추가와 제거
-        // nativeElement.getBoundingClientRect() 요소의 좌표값을 찾아줌.
-        this.visualTop = this.wrapRef.nativeElement.getBoundingClientRect().top;
-        this.contentsTop = this.contentsRef.nativeElement.getBoundingClientRect().top;
-        this.worksCardTop = this.worksCardRef.nativeElement.getBoundingClientRect().top;
-    }
-
-    ngAfterViewInit(): void {
-        const textToSplit = document.getElementsByClassName('textToSplit');
-        Splitting({ target: textToSplit });
+        AOS.init();
     }
 
     @HostListener('scroll', ['$event'])
@@ -54,20 +24,6 @@ export class IndexComponent implements OnInit, AfterViewInit {
         } else {
             this.isHeaderActive = false;
         }
-
-        this.isVisualActive = nowScrollTop === this.visualTop;
-        console.log('this.isVisualActive 클래스 네임 변경', this.isVisualActive);
-        console.log('visualTop:', this.visualTop);
-
-        this.isContentsActive = nowScrollTop === this.contentsTop;
-        console.log('this.isContentsActive 클래스 네임 변경', this.isContentsActive);
-        console.log('contentsTop:', this.contentsTop);
-
-        this.isworksCardsActive = nowScrollTop === this.worksCardTop;
-        console.log('this.isworksCardsActive 클래스 네임 변경', this.isworksCardsActive);
-        console.log('worksCardTop:', this.worksCardTop);
-
-        console.log('nowScrollTop', nowScrollTop);
 
         this.prevScrollTop = nowScrollTop;
     }
